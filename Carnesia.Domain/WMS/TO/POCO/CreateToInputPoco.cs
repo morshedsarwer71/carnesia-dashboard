4be61ad0faf35afee24a25aa06b927eec33baafd
nafismuhymeen <muhymeennafis@gmail.com>
2022-04-12 22:55:46 +0600

    Create TO Class

---
+++ Carnesia.Domain/WMS/TO/POCO/CreateToInputPoco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.WMS.TO.POCO
+{
+    public class CreateToInputPoco
+    {
+        public string SourceWarehouse { get; set; }
+        public string DestinationWarehouse { get; set; }
+        public string SKU { get; set; }
+        public int QTY { get; set; }
+    }
+}
