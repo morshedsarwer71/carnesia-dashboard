00e3ffda2f396d5a16dc6f19cdabb3f560ec257d
morshed sarwer <morshedsarwer@gmail.com>
2022-04-17 02:58:30 +0600

    excel upload added

--- Carnesia.Application/WMS/PO/Services/IPurchaseOrder.cs
+++ Carnesia.Application/WMS/PO/Services/IPurchaseOrder.cs
@@ -1,16 +1,20 @@
 ﻿using Carnesia.Domain.WMS.PO.Models;
 using Carnesia.Domain.WMS.PO.ViewModels;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Carnesia.Application.WMS.PO.Services
 {
     public interface IPurchaseOrder
     {
         Task<string> AddPurchaseOrder(PurchaseOrder purchaseOrder);
-        Task<MemoryStream> DownLoadExcel();
+        Task DownLoadExcel(IJSRuntime jSRuntime);
+        Task<List<PoProductDTO>> UploadExcelFile(IBrowserFile file);
     }
 }
