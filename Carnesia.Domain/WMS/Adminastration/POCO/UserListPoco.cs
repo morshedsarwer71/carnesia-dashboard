327abadfd08a8ec2a1cb3bb388fc2d2441bc7064
nafismuhymeen <muhymeennafis@gmail.com>
2022-04-16 17:15:33 +0600

    User List

---
+++ Carnesia.Domain/WMS/Adminastration/POCO/UserListPoco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.WMS.Adminastration.POCO
+{
+    public class UserListPoco
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string userType { get; set; }
+        public bool isActive { get; set; }
+    }
+}
