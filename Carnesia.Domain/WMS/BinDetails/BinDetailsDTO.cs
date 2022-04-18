b3ba3552da5f4ad04c15c3401758de3a17383309
nafismuhymeen <muhymeennafis@gmail.com>
2022-04-19 05:10:30 +0600

    Bin Inscan, Bin details, Track UID

---
+++ Carnesia.Domain/WMS/BinDetails/BinDetailsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.WMS.BinDetails
+{
+    public class BinDetailsDTO
+    {
+        public string ItemCode { get; set; }
+        public string ProductName { get; set; }
+        public string BIN { get; set; }
+        public int QTY { get; set; }
+    }
+}
---
+++ Carnesia.Domain/WMS/TrackUID/TrackUIDDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.WMS.TrackUID
+{
+    public class TrackUIDDTO
+    {
+        public string? POID { get; set; }
+        public string? OrderID { get; set; }
+        public string? TOID { get; set; }
+        public string? PickListID { get; set; }
+        public string? BIN { get; set; }
+        public string LastStatus { get; set; }
+        public string StatusUpdateDate { get; set; }
+        public string StatusUpdatedBy { get; set; }
+    }
+}
