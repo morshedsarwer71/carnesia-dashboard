300494ea4cb9e2ed83484417ea907a480bc649f2
nafismuhymeen <muhymeennafis@gmail.com>
2022-04-20 02:55:28 +0600

    Bin Transfer, View Details Return List, Return Acknowledgement

---
+++ Carnesia.Domain/WMS/BinTransfer/BinTransferDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.WMS.BinTransfer
+{
+    public class BinTransferDTO
+    {
+        public string ItemCode { get; set; }
+        public string ProductName { get; set; }
+        public string BIN { get; set; }
+        public string Image { get; set; }
+        public int QTY { get; set; }
+    }
+}
---
+++ Carnesia.Domain/WMS/ReturnManagement/POCO/ViewDetailsForReturnListPoco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.WMS.ReturnManagement.POCO
+{
+    public class ViewDetailsForReturnListPoco
+    {
+        public string SKU { get; set; }
+        public string ProductName { get; set; }
+        public int QTY { get; set; }
+        public int LiftingPrice { get; set; }
+        public int TotalCost { get; set; }
+    }
+}
