33fd46dfd7a7cec241a85962f491ba266b701e25
nafismuhymeen <muhymeennafis@gmail.com>
2022-04-22 00:08:37 +0600

    Pannel Adjustment

--- Carnesia.Domain/CMS/Models/ProductDTO.cs
+++ Carnesia.Domain/CMS/Models/ProductDTO.cs
@@ -9,9 +9,11 @@ public class ProductDTO
     public string weight { get; set; }
     public string productDimension { get; set; }
     public int brandId { get; set; }
+    public string brandName { get; set; }
     public bool isPreOrder { get; set; }
     public bool isPublish { get; set; }
     public bool isHasFreeProd { get; set; }
-    public int delivaryDayPreorder { get; set; }
+    public int? delivaryDayPreorder { get; set; }
     public string description { get; set; }
+    public decimal? mrp { get; set; }
 }
