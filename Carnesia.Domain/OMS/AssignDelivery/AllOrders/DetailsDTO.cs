38a985421b38ab3f44b2c4b36d7169fe57a75c44
nafismuhymeen <muhymeennafis@gmail.com>
2022-05-13 22:08:46 +0600

    Assign Delivery

---
+++ Carnesia.Domain/OMS/AssignDelivery/AllOrders/DetailsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.OMS.AssignDelivery.AllOrders
+{
+    public class DetailsDTO
+    {
+        public string SKU { get; set; }
+        public string ProductName { get; set; }
+        public string Status { get; set; }
+        public int QTY { get; set; }
+        public decimal Price { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Carnesia.Domain/OMS/AssignDelivery/AllOrders/HistoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.OMS.AssignDelivery.AllOrders
+{
+    public class HistoryDTO
+    {
+        public string Status { get; set; }
+        public string UpdatedOn { get; set; }
+        public string Comments { get; set; }
+        public string AttendedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public string Hub { get; set; }
+        public string Instruction { get; set; }
+    }
+}
