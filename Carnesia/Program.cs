2b758b5612a512f599428fb7fed57aa3ab17b4a7
morshed sarwer <morshedsarwer@gmail.com>
2022-06-19 15:24:22 +0600

    blazor liecense key added

--- Carnesia/Program.cs
+++ Carnesia/Program.cs
@@ -32,14 +32,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Carnesia.Application.WMS.Bin.Services;
 using Carnesia.Application.WMS.PutAway.Services;
+using Syncfusion.Blazor;
 
+
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NjU4NDc3QDMyMzAyZTMxMmUzMEwvOTZmTHFNNFFNb1BIdURQazB5NWpCYXpvbzNQYzFIZnZrRldyZFlOVGc9");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://carnesiaapi.bespokeit.io/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7090/api/") });
 builder.Services.AddScoped<IPurchaseOrder, PurchaseOrderService>();
 builder.Services.AddScoped<IVendor, VendorServices>();
 builder.Services.AddScoped<IStore, StoreService>();
