b3ead650c107f5ca5b690952ddf75bb0825e8290
nafismuhymeen <muhymeennafis@gmail.com>
2022-07-06 06:55:41 +0600

    Generate Bill

---
+++ Carnesia.Domain/Dashboard/CustomerProfile/CustomerProfileDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.Dashboard.CustomerProfile
+{
+    public class CustomerProfileDTO
+    {
+        public string registerDate { get; set; }
+        public string customerId { get; set; }
+        public string membershipId { get; set; }
+        public string customerName { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+    }
+}
---
+++ Carnesia.Domain/Dashboard/GenerateBill/GenerateBillProductTableDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.Dashboard.GenerateBill
+{
+    public class GenerateBillProductTableDTO
+    {
+        public string productName { get; set; }
+        public string productSKU { get; set; }
+        public decimal mrp { get; set; }
+        public decimal sellingPrice { get; set; }
+        public int qty { get; set; }
+    }
+}
