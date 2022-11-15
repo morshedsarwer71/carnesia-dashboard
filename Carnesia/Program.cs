f33880b2101631a20fbf431aceff9d3f621f6e6a
Morshed Sarwer Sikder <morshedsarwer71@gmail.com>
2022-11-15 14:50:07 +0600

    image page url

--- Carnesia/Program.cs
+++ Carnesia/Program.cs
@@ -78,13 +78,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cscpguide.com/api/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://oshud.com/api/") });
 
 //meesagehandler
 
 builder.Services.AddHttpClient("RetailSuite", client =>
 {
-    client.BaseAddress = new Uri("https://cscpguide.com/api/");
+    client.BaseAddress = new Uri("https://oshud.com/api/");
 }).AddHttpMessageHandler<AuthorizationMessageHandler>();
 
 builder.Services.RemoveAll<IHttpMessageHandlerBuilderFilter>();
