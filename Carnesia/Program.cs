5c2da40cf2922d5501ff162a48d61ac94a2a8417
nafismuhymeen <muhymeennafis@gmail.com>
2022-11-21 16:28:32 +0600

    url change

--- Carnesia/Program.cs
+++ Carnesia/Program.cs
@@ -78,13 +78,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7090/api/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://cscpguide.com/api/") });
 
 //meesagehandler
 
 builder.Services.AddHttpClient("RetailSuite", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7090/api/");
+    client.BaseAddress = new Uri("https://cscpguide.com/api/");
 }).AddHttpMessageHandler<AuthorizationMessageHandler>();
 
 builder.Services.RemoveAll<IHttpMessageHandlerBuilderFilter>();
