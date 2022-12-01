826f2e557451f2adcce8c7c50330882aef01133c
wahidul islam <wahid.cse38@gmail.com>
2022-12-01 21:48:49 +0600

    design product revenue and profit

---
+++ Carnesia.Domain/MIS/Revenue/ProdRevenueAndProfitDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carnesia.Domain.MIS.Revenue
+{
+    public class ProdRevenueAndProfitDTO
+    {
+        public string bu { get; set; }
+        public string ecomOrOutlet { get; set; }
+        public string childCat { get; set; }
+        public string brand { get; set; }
+        public string productCode { get; set; }
+        public string sku { get; set; }
+        public string productName { get; set; }
+        public string soldQty { get; set; }
+        public string sellingPrice { get; set; }
+        public string soldValue { get; set; }
+        public string totalCost { get; set; }
+        public string profit { get; set; }
+    }
+
+    public class TotalRevProfitDTO
+    {
+        public string soldTotal { get; set; }
+        public string product { get; set; }
+        public string quantity { get; set; }
+        public string totalCost { get; set; }
+        public string profit { get; set; }
+    }
+}
