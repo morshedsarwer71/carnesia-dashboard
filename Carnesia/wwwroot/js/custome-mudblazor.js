fa1806a9d142ca4a7a62f74c3f0e663d17886399
nafismuhymeen <muhymeennafis@gmail.com>
2025-09-08 01:28:37 +0600

    Dummy Changes

---
+++ Carnesia/wwwroot/js/custome-mudblazor.js
@@ -0,0 +1,9 @@
+﻿window.mudDragAndDrop = {
+    initDropZone: id => {
+        const elem = document.getElementById("mud-drop-zone-" + id);
+        if (elem) {
+            elem.addEventListener("dragover", (event) => event.preventDefault());
+            elem.addEventListener("dragstart", (event) => event.dataTransfer.setData("", event.target.id));
+        }
+    }
+};
